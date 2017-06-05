716770feeb9bcd542e53dd5532613779b3e3913f
Yannis Gravezas <wizgrav@gmail.com>
2017-06-05 22:26:53 +0300

    bumped dist

--- dist/aframe-effects.js
+++ dist/aframe-effects.js
@@ -767,10 +767,10 @@
 	    diffuse: true,
 
 	    sobel: [
-			"mat3 G[2];",
+			"mat3 $G[2];",
 
-			"const mat3 g0 = mat3( 1.0, 2.0, 1.0, 0.0, 0.0, 0.0, -1.0, -2.0, -1.0 );",
-			"const mat3 g1 = mat3( 1.0, 0.0, -1.0, 2.0, 0.0, -2.0, 1.0, 0.0, -1.0 );",
+			"const mat3 $g0 = mat3( 1.0, 2.0, 1.0, 0.0, 0.0, 0.0, -1.0, -2.0, -1.0 );",
+			"const mat3 $g1 = mat3( 1.0, 0.0, -1.0, 2.0, 0.0, -2.0, 1.0, 0.0, -1.0 );",
 
 
 			"void $main(inout vec4 color, vec4 origColor, vec2 uv, float depth) {",
@@ -779,8 +779,8 @@
 				"float cnv[2];",
 				"float d;",
 
-				"G[0] = g0;",
-				"G[1] = g1;",
+				"$G[0] = $g0;",
+				"$G[1] = $g1;",
 
 				"for (float i=0.0; i<3.0; i++)",
 				"for (float j=0.0; j<3.0; j++) {",
@@ -790,7 +790,7 @@
 				"}",
 
 				"for (int i=0; i<2; i++) {",
-					"float dp3 = dot(G[i][0], I[0]) + dot(G[i][1], I[1]) + dot(G[i][2], I[2]);",
+					"float dp3 = dot($G[i][0], I[0]) + dot($G[i][1], I[1]) + dot($G[i][2], I[2]);",
 					"cnv[i] = dp3 * dp3; ",
 				"}",
 				"color = vec4($color, sqrt(cnv[0]*cnv[0]+cnv[1]*cnv[1]));",
